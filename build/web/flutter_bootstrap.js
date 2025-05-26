a48a1bb98eeab800e1c37fc648953da7cc3e6e5b
DESKTOP-IVA2838\SC3 <awais.squadcell@gmail.com>
2025-05-26 20:38:26 +0500

    for vercel only

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"82bd5b7209295a5b7ff8cae0df96e7870171e3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2963603328"
+    serviceWorkerVersion: "2489257375"
   }
 });
